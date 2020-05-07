95e690c6b1436d078f8f4f9e2973f4626d59ed33
Michael C <mcal5071@gmail.com>
2020-05-07 12:26:58 +1200

    Feat: Add GetWikiSubDirectory method.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace WikiSeasonRetriever
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Wikipedia URL: " );
+            string wikiURL = Console.ReadLine().Trim();
+
+            string pageAttribute = GetWikiSubdirectory(wikiURL);
+
+            Console.WriteLine(pageAttribute);
+        }
+
+        static string GetWikiSubdirectory(string wikiURL)
+        {
+            int indexShift = 5; //Takes account of "wiki/" spaces.
+
+            int number = wikiURL.IndexOf("wiki/") + indexShift;
+            
+            return wikiURL.Substring(number);
+        }
+    }
+}
