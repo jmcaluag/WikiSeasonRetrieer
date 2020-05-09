3863fcff1d1f4cdc748298bc18165e38f2da3764
Michael C <mcal5071@gmail.com>
2020-05-09 15:42:42 +1200

    Change: Modify for properties naming convention

--- WikiSection.cs
+++ WikiSection.cs
@@ -1,25 +1,47 @@
 using System.Collections.Generic;
+using System.Text.Json.Serialization;
 
 class WikiSection
 {
-    public Parse parse { get; set; }
+    [JsonPropertyName("parse")]
+    public SectionParse SectionParse { get; set; }
 }
 
-class Parse
+class SectionParse
 {
-    public string title { get; set; }
-    public int pageid { get; set; }
-    public List<Section> sections { get; set; }
+    [JsonPropertyName("title")]
+    public string Title { get; set; }
+    
+    [JsonPropertyName("pageid")]
+    public int PageId { get; set; }
+    
+    [JsonPropertyName("sections")]
+    public List<Section> Sections { get; set; }
 }
 
  public class Section
 {
-    public int toclevel { get; set; }
-    public string level { get; set; }
-    public string line { get; set; }
-    public string number { get; set; }
-    public string index { get; set; }
+    [JsonPropertyName("toclevel")]
+    public int TocLevel { get; set; }
+    
+    [JsonPropertyName("level")]
+    public string Level { get; set; }
+    
+    [JsonPropertyName("line")]
+    public string Line { get; set; }
+    
+    [JsonPropertyName("number")]
+    public string Number { get; set; }
+    
+    [JsonPropertyName("index")]
+    public string Index { get; set; }
+    
+    [JsonPropertyName("fromtitle")]
     public string fromtitle { get; set; }
-    public int byteoffset { get; set; }
-    public string anchor { get; set; }
+    
+    [JsonPropertyName("byteoffset")]
+    public int ByteOffSet { get; set; }
+    
+    [JsonPropertyName("anchor")]
+    public string Anchor { get; set; }
 }
